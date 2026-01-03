a4f88041e9773fbcc96abba99349bfd17ecbff3f
Akshit <gakshit92@gmail.com>
2026-01-03 20:32:27 +0530

    feat: add Lender model, database migration, and initial API endpoint

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.base import Base
+from app.models.lender import Lender
 
 # Add other models here as you create them
-# from app.models.item import Item 
+# from app.models.item import Item

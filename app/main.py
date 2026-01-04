0afc063932d82a6ce8f63277b0d2c076ccfce1ad
Akshit <gakshit92@gmail.com>
2026-01-05 03:17:45 +0530

    feat: Implement initial FastAPI application with CORS, API routing, and database connection disposal.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.v1.api import api_router
 
 from contextlib import asynccontextmanager
 from app.core.database import engine
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,13 +14,24 @@ async def lifespan(app: FastAPI):
     # Shutdown
     await engine.dispose()
 
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    lifespan=lifespan
+    lifespan=lifespan,
+)
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
 
 @app.get("/")
 async def root():
